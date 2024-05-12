ec3bd9d1aafb57b60901a4a42597ca9d5cb07b05
Le Vivilet <levivlet@pm.me>
2024-05-12 21:48:21 +0200

    feature: add setIframeSrcFallback function to set iframe src in case of error

--- src/parts/CommandMap/CommandMap.js
+++ src/parts/CommandMap/CommandMap.js
@@ -1,6 +1,6 @@
-import * as HandleElectronMessagePort from '../HandleElectronMessagePort/HandleElectronMessagePort.js'
-import * as ElectronWebContentsView from '../ElectronWebContentsView/ElectronWebContentsView.js'
 import * as ElectronWebContents from '../ElectronWebContents/ElectronWebContents.js'
+import * as ElectronWebContentsView from '../ElectronWebContentsView/ElectronWebContentsView.js'
+import * as HandleElectronMessagePort from '../HandleElectronMessagePort/HandleElectronMessagePort.js'
 
 export const commandMap = {
   'ElectronWebContentsView.backward': ElectronWebContents.backward,
@@ -14,6 +14,7 @@ export const commandMap = {
   'ElectronWebContentsView.reload': ElectronWebContents.reload,
   'ElectronWebContentsView.resizeBrowserView': ElectronWebContentsView.resizeWebContentsView,
   'ElectronWebContentsView.setIframeSrc': ElectronWebContentsView.setIframeSrc,
+  'ElectronWebContentsView.setIframeSrcFallback': ElectronWebContentsView.setIframeSrcFallback,
   'ElectronWebContentsView.show': ElectronWebContentsView.show,
   'ElectronWebContents.handleDidNavigate': ElectronWebContentsView.handleDidNavigate,
   'ElectronWebContents.handleTitleUpdated': ElectronWebContentsView.handleTitleUpdated,
