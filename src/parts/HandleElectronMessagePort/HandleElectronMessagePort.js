ce52d1f40530a01679d1716ae3e51007dca6d890
Le Vivilet <levivlet@pm.me>
2024-05-13 23:08:39 +0200

    feature: remove unused code

--- src/parts/HandleElectronMessagePort/HandleElectronMessagePort.js
+++ src/parts/HandleElectronMessagePort/HandleElectronMessagePort.js
@@ -5,16 +5,4 @@ export const handleElectronMessagePort = async (messagePort, ipcId) => {
   Assert.object(messagePort)
   Assert.number(ipcId)
   return HandleIncomingIpc.handleIncomingIpc(ipcId, messagePort, {})
-  // const ipc = await IpcChild.listen({
-  //   method: IpcChildType.ElectronMessagePort,
-  //   messagePort,
-  // })
-  // HandleIpc.handleIpc(ipc)
-  // if (ipcId === IpcId.MainProcess) {
-  //   ParentIpc.state.ipc = ipc
-  // } else if (ipcId === IpcId.SharedProcess) {
-  //   SharedProcessIpc.state.ipc = ipc
-  // } else if (ipcId === IpcId.EmbedsWorker) {
-  //   ipc.addEventListener('close', HandleIpcClosed.handleIpcClosed)
-  // }
 }
