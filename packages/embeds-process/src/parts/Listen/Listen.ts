b78b8396856cfc6ee7fdb245ec26686772578023
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-08 23:23:49 +0200

    feature: assign commands to commandMapref (#49)

--- packages/embeds-process/src/parts/Listen/Listen.ts
+++ packages/embeds-process/src/parts/Listen/Listen.ts
@@ -1,8 +1,10 @@
 import * as CommandMap from '../CommandMap/CommandMap.ts'
+import * as CommandMapRef from '../CommandMapRef/CommandMapRef.ts'
 import * as IpcChild from '../IpcChild/IpcChild.ts'
 import * as IpcChildType from '../IpcChildType/IpcChildType.ts'
 
 export const listen = async (argv: readonly string[]): Promise<void> => {
+  Object.assign(CommandMapRef.commandMapRef, CommandMap.commandMap)
   await IpcChild.listen({
     method: IpcChildType.Auto(argv),
     commandMap: CommandMap.commandMap,
