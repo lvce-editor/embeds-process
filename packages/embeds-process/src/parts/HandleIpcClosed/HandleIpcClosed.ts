35e17b07611a3b013a8c4f5b41f1010e320d0876
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-08 23:10:21 +0200

    feature: add ipc closed function (#38)

---
+++ packages/embeds-process/src/parts/HandleIpcClosed/HandleIpcClosed.ts
@@ -0,0 +1,23 @@
+import * as ElectronWebContentsView from '../ElectronWebContentsView/ElectronWebContentsView.ts'
+import * as ElectronWebContentsViewIpcState from '../ElectronWebContentsViewIpcState/ElectronWebContentsViewIpcState.ts'
+
+const getIdsToDispose = (ipc: any) => {
+  const entries = ElectronWebContentsViewIpcState.getAll()
+  const toDispose = []
+
+  for (const [id, value] of entries) {
+    if (value === ipc) {
+      toDispose.push(parseInt(id))
+    }
+  }
+  return toDispose
+}
+
+export const handleIpcClosed = async (event: any) => {
+  const idsToDispose = getIdsToDispose(event.target)
+  for (const id of idsToDispose) {
+    ElectronWebContentsViewIpcState.remove(id)
+    await ElectronWebContentsView.disposeWebContentsView(id)
+  }
+  // SharedProcessIpc.send('HandleMessagePortForEmbedsProcess.handleEmbedsProcessIpcClosed')
+}
