23292d867d3e80d944bcf21fb5e6e0ccc88bcd33
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-08 23:59:52 +0200

    feature: use monorepo (#58)
    * feature: use monorepo
    
    * tsc

--- src/parts/HandleElectronMessagePort/HandleElectronMessagePort.js
+++
@@ -1,8 +0,0 @@
-import * as Assert from '../Assert/Assert.js'
-import * as HandleIncomingIpc from '../HandleIncomingIpc/HandleIncomingIpc.js'
-
-export const handleElectronMessagePort = async (messagePort, ipcId) => {
-  Assert.object(messagePort)
-  Assert.number(ipcId)
-  return HandleIncomingIpc.handleIncomingIpc(ipcId, messagePort, {})
-}
