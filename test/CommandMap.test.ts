74d3337b4d9b7820ec586d926cf1bc1b81632880
Le Vivilet <levivlet@pm.me>
2025-05-08 23:52:40 +0200

    feature: use monorepo

--- test/CommandMap.test.ts
+++
@@ -1,6 +0,0 @@
-import * as CommandMap from '../src/parts/CommandMap/CommandMap.js'
-import { test, expect } from '@jest/globals'
-
-test('commandMap', () => {
-  expect(typeof CommandMap.commandMap).toBe('object')
-})
