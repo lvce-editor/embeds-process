27674b0a3a218e0eb7061717e48eb9adf5ae2382
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-08 23:22:47 +0200

    feature: add remaining embeds functions (#48)

--- packages/embeds-process/src/parts/CommandMap/CommandMap.ts
+++ packages/embeds-process/src/parts/CommandMap/CommandMap.ts
@@ -1,1 +1,27 @@
-export const commandMap = {}
+import * as ElectronWebContents from '../ElectronWebContents/ElectronWebContents.ts'
+import * as ElectronWebContentsView from '../ElectronWebContentsView/ElectronWebContentsView.ts'
+import * as HandleElectronMessagePort from '../HandleElectronMessagePort/HandleElectronMessagePort.ts'
+
+export const commandMap = {
+  'ElectronWebContentsView.backward': ElectronWebContents.backward,
+  'ElectronWebContentsView.createWebContentsView': ElectronWebContentsView.createWebContentsView,
+  'ElectronWebContentsView.disposeWebContentsView': ElectronWebContentsView.disposeWebContentsView,
+  'ElectronWebContentsView.focus': ElectronWebContents.focus,
+  'ElectronWebContentsView.forward': ElectronWebContents.forward,
+  'ElectronWebContentsView.getStats': ElectronWebContentsView.getStats,
+  'ElectronWebContentsView.inspectElement': ElectronWebContents.inspectElement,
+  'ElectronWebContentsView.openDevtools': ElectronWebContents.openDevtools,
+  'ElectronWebContentsView.reload': ElectronWebContents.reload,
+  'ElectronWebContentsView.resizeBrowserView': ElectronWebContentsView.resizeWebContentsView,
+  'ElectronWebContentsView.setIframeSrc': ElectronWebContentsView.setIframeSrc,
+  'ElectronWebContentsView.setIframeSrcFallback': ElectronWebContentsView.setIframeSrcFallback,
+  'ElectronWebContentsView.setFallthroughKeyBindings': ElectronWebContentsView.setFallthroughKeyBindings,
+  'ElectronWebContentsView.handleKeyBinding': ElectronWebContentsView.handleKeyBinding,
+  'ElectronWebContentsView.show': ElectronWebContentsView.show,
+  'ElectronWebContents.handleDidNavigate': ElectronWebContentsView.handleDidNavigate,
+  'ElectronWebContents.handleTitleUpdated': ElectronWebContentsView.handleTitleUpdated,
+  'ElectronWebContents.handleBrowserViewDestroyed': ElectronWebContentsView.handleBrowserViewDestroyed,
+  'ElectronWebContents.handleWillNavigate': ElectronWebContentsView.handleWillNavigate,
+  'ElectronWebContents.handleContextMenu': ElectronWebContentsView.handleContextMenu,
+  'HandleElectronMessagePort.handleElectronMessagePort': HandleElectronMessagePort.handleElectronMessagePort,
+}
---
+++ packages/embeds-process/src/parts/HandleIpcModule/HandleIpcModule.ts
@@ -0,0 +1,19 @@
+import * as Assert from '../Assert/Assert.ts'
+import * as HandleIpcEmbedsWorker from '../HandleIpcEmbedsWorker/HandleIpcEmbedsWorker.ts'
+import * as HandleIpcMainProcess from '../HandleIpcMainProcess/HandleIpcMainProcess.ts'
+import * as HandleIpcSharedProcess from '../HandleIpcSharedProcess/HandleIpcSharedProcess.ts'
+import * as IpcId from '../IpcId/IpcId.ts'
+
+export const getModule = (ipcId: any) => {
+  Assert.number(ipcId)
+  switch (ipcId) {
+    case IpcId.SharedProcess:
+      return HandleIpcSharedProcess
+    case IpcId.EmbedsWorker:
+      return HandleIpcEmbedsWorker
+    case IpcId.MainProcess:
+      return HandleIpcMainProcess
+    default:
+      throw new Error(`Embeds process encountered unexpected incoming ipc ${ipcId}`)
+  }
+}
