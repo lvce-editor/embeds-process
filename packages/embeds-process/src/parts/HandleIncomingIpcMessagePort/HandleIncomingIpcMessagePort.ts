9f997eead0a836566c758cc0d093728a03a5ca86
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-08 23:12:59 +0200

    feature: add handleMessagePort (#39)

---
+++ packages/embeds-process/src/parts/HandleIncomingIpcMessagePort/HandleIncomingIpcMessagePort.ts
@@ -0,0 +1,8 @@
+export const handleIncomingIpcMessagePort = async (module: any, handle: any, message: any) => {
+  const target = await module.targetMessagePort(handle, message)
+  const response = module.upgradeMessagePort(handle, message)
+  return {
+    target,
+    response,
+  }
+}
