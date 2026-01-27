cede7ec421f548dd4311c8acef20264195119130
Le Vivilet <levivilet@pm.me>
2026-01-28 00:35:25 +0100

    fix

--- packages/embeds-process/src/parts/HandleIpcModule/HandleIpcModule.ts
+++ packages/embeds-process/src/parts/HandleIpcModule/HandleIpcModule.ts
@@ -7,13 +7,13 @@ import * as IpcId from '../IpcId/IpcId.ts'
 export const getModule = (ipcId: number) => {
   Assert.number(ipcId)
   switch (ipcId) {
-    case IpcId.SharedProcess:
-      return HandleIpcSharedProcess // deprecated
     case IpcId.EmbedsWorker:
     case IpcId.EmbedsWorkerAlt:
       return HandleIpcEmbedsWorker
     case IpcId.MainProcess:
       return HandleIpcMainProcess // deprecated
+    case IpcId.SharedProcess:
+      return HandleIpcSharedProcess // deprecated
     default:
       throw new Error(`Embeds process encountered unexpected incoming ipc ${ipcId}`)
   }
