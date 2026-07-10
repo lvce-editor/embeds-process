675dd15f311fc7af81b37c9bf724524d53ddcc51
Le Vivilet <levivilet@pm.me>
2026-07-10 12:48:43 +0000

    feature: update eslint config

--- packages/embeds-process/src/parts/ElectronWebContentsView/ElectronWebContentsView.ts
+++ packages/embeds-process/src/parts/ElectronWebContentsView/ElectronWebContentsView.ts
@@ -1,3 +1,4 @@
+// cspell:ignore webcontents
 import * as Assert from '../Assert/Assert.ts'
 import * as ElectronWebContents from '../ElectronWebContents/ElectronWebContents.ts'
 import * as ElectronWebContentsViewIpcState from '../ElectronWebContentsViewIpcState/ElectronWebContentsViewIpcState.ts'
